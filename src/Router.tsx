f2e9b66f88aba50b8239a75057c14980116982e8
Brayan Josue Hernandez Saravia <68264371+brayan200014@users.noreply.github.com>
2022-12-01 21:10:32 -0600

    Create Router.tsx

---
+++ src/Router.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+  Navigate,
+} from "react-router-dom";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+       
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
