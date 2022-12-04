451859ac562e2166f4479cd374b646502038dfee
brayan200014 <josue200014@outlook.com>
2022-12-03 21:01:48 -0600

    Diseño SignIn

--- src/components/inputFloating.tsx
+++ src/components/inputFloating.tsx
@@ -1,6 +1,14 @@
+import { stringify } from "querystring";
 import React from "react";
-import { text } from "stream/consumers";
+import { JSON } from "sequelize/dist";
+import { arrayBuffer, json, text } from "stream/consumers";
 
+interface IError  {
+    value: string,
+      msg: string,
+      param: string,
+      location: string
+}
 
 
 const FloatingInput= ({
@@ -8,15 +16,56 @@ const FloatingInput= ({
     labelText = "",
     type = "text",
     value = "",
+    error= "",
     onChange = (e:React.ChangeEvent<HTMLInputElement>) => { },
     ...rest
 }) => {
-    return (
-        <div className="relative z-0 m-3 p-2">
-        <input type={type} id="default_standard" value={value} name={name} onChange= {onChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-        <label htmlFor={name} className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{labelText}</label>
-    </div>
-    )
+    if(error=="") {
+        return (
+            <div className="relative z-0 m-3 p-2">
+            <input type={type} id="default_standard" value={value} name={name} onChange= {onChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor={name} className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{labelText}</label>
+            <span>{"udefin"}</span>
+        </div>
+        )
+    } else {
+        let arr= Object.values(error);
+        let arrError= Object.values(arr[1]);
+        console.log(arrError);
+       
+      
+           arrError.map((x,i)=>{
+            console.log(x[i]);
+            let result= Object.values(x[i]);
+            console.log(result[2]);
+                if(result[2]== "email") {
+                return (
+                    <div className="relative z-0 m-3 p-2">
+                    <input type={type} id="default_standard" value={value} name={name} onChange= {onChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label htmlFor={name} className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{labelText}</label>
+                    <span key={i}>{result[1]}</span>
+                    </div>
+                 )
+                } 
+
+                
+           })
+
+           return (
+            <div className="relative z-0 m-3 p-2">
+            <input type={type} id="default_standard" value={value} name={name} onChange= {onChange} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <label htmlFor={name} className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">{labelText}</label>
+            <span >{"Hola"}</span>
+            </div>
+         )
+
+           
+        
+    } 
+    
+ 
+       
+    
 }
 
 export default FloatingInput;
--- src/views/SignIn/SignInUx.tsx
+++ src/views/SignIn/SignInUx.tsx
@@ -1,11 +1,19 @@
 
 import FloatingInput  from '@components/inputFloating';
+import { typeImplementation } from '@testing-library/user-event/dist/type/typeImplementation';
 import e from 'express';
 import { useState } from 'react';
 import DatePicker from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
 
+import "react-datepicker/dist/react-datepicker.css";
+import { json } from 'stream/consumers';
+/*export interface IError {
+    value: string,
+    msg: string,
+    param: string,
+    location: string
+}*/
 export interface IloginUxProps {
     name: string, 
     setName: (name: string) => void,
@@ -19,9 +27,14 @@ export interface IloginUxProps {
     setPasswordConfirm: (password: string) => void,
     birthDate: string,
     setBirthDate: (birthDate: string)=> void,
-    error: any,
+    error?: any,
     handleClick: () => void
 }
+
+
+
+
+
 
 const SignInUx = ({
     name,
@@ -40,31 +53,42 @@ const SignInUx = ({
     handleClick
 }: IloginUxProps) => {
 
+
+    
     return (
-        <div className=' flex justify-center items-center'>
+        <div className='z-40 max-h-screen flex justify-center  -translate-y-6 items-center bg-gradient-to-br from-purple-600 to-blue-500 '>
+            
             <form>
-            <div className=' flex flex-col mt-10'>
-                <h3>Sign In</h3>
-               
+            <div className=' z-30 flex flex-col mt-10 pb-15  translate-y-8  bg-white rounded-lg border border-green-200 shadow-lg shadow-green-300  '>
+                <h3 className='m-4 text-center text-dark font-medium rounded-lg text-base'>Sign In</h3>
+
                 <FloatingInput
                     name="name"
                     labelText='Name'
                     value= {name}
+                    error= {error}
                     onChange= {(e) => setName(e.target.value)}
                     type= 'text'
                 />
+            
                   <FloatingInput
                     name="email"
                     labelText='Email'
                     value= {email}
+                    error={error}
                     onChange= {(e) => setEmail(e.target.value)}
                     type= 'email'
                 />
+
+                {
+                    
+                }
                
                 <FloatingInput
                     name="username"
                     labelText='Username'
                     value= {username}
+                    error={error}
                     onChange= {(e) => setUsername(e.target.value)}
                     type= 'text'
                 />
@@ -73,6 +97,7 @@ const SignInUx = ({
                     name="Password"
                     labelText='Password'
                     value= {password}
+                    error={error}
                     onChange= {(e) => setPassword(e.target.value)}
                     type= 'password'
                 />
@@ -80,6 +105,7 @@ const SignInUx = ({
                     name="Confirm Password"
                     labelText='Confirm Password'
                     value= {passwordConfirm}
+                    error={error}
                     onChange= {(e) => setPasswordConfirm(e.target.value)}
                     type= 'password'
                 />
@@ -89,14 +115,17 @@ const SignInUx = ({
                     labelText="Fecha"
                     value={birthDate}
                     type= "date"
+                    error={error}
                     onChange={(e) => setBirthDate(e.target.value)}
                     />
            
-                <button className='place-self-end' onClick={ async (e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    handleClick();
-                }}>Probar</button>
+           <button 
+           onClick={ async (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            handleClick();
+        }}
+           className="text-white place-self-end bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Sign In</button>
             </div>
             </form>
             </div>
